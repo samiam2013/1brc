09df951e3b78e25542422d47eaffb4447eab5f7a
Samuel Myres <samiam2013@gmail.com>
2024-03-24 13:25:24 -0500

    copy weather station names, start util for 1br file gen

---
+++ cmd/gen1br/main.go
@@ -0,0 +1,11 @@
+package main
+
+
+func main() {
+	// read in the names from weather_stations.csv to a map
+
+	// make a file 1brc.txt
+	//  randomly pick a weather station
+	// generate a normally distributed temperature in Celcius between -99.9 and 99.9
+	
+}
