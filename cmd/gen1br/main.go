1855c232dc0fc67cd5c262612c7aa4ff123a4721
samiam2013 <samiam2013@gmail.com>
2024-03-24 14:41:24 -0500

    Add go.mod and go.sum files, and implement logic to generate 1brc.txt file with weather station names and temperature values

--- cmd/gen1br/main.go
+++ cmd/gen1br/main.go
@@ -1,11 +1,48 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+
+	"github.com/sirupsen/logrus"
+)
 
 func main() {
 	// read in the names from weather_stations.csv to a map
+	fh, err := os.Open("weather_stations.csv")
+	if err != nil {
+		logrus.WithError(err).Fatal("could not open weather_stations.csv. is it in the current directory?")
+	}
+	defer func() { _ = fh.Close() }()
+
+	wsNames := make(map[int]string, 42_000)
+	scanner := bufio.NewScanner(fh)
+	for i := 0; scanner.Scan(); i++ {
+		name := scanner.Text()
+		wsNames[i] = name
+	}
+	wsCount := len(wsNames)
+	logrus.Infof("read %d weather station names", len(wsNames))
 
 	// make a file 1brc.txt
-	//  randomly pick a weather station
-	// generate a normally distributed temperature in Celcius between -99.9 and 99.9
-	
+	fh, err = os.Create("1brc.txt")
+	if err != nil {
+		logrus.WithError(err).Fatal("could not create 1brc.txt")
+	}
+	defer func() { _ = fh.Close() }()
+
+	// randomly pick a weather station
+	// 	generate a normally distributed temperature in Celcius
+	//	between -99.9 and 99.9
+	for lTW := 0; lTW < 1_000_000_000; lTW++ {
+		line := wsNames[rand.Intn(wsCount)] + ";" + fmt.Sprintf("%.1f\n", rand.NormFloat64()*20+19)
+		if _, err := fh.WriteString(line); err != nil {
+			logrus.WithError(err).Fatal("could not write to 1brc.txt")
+		}
+		if lTW%10_00_000 == 0 {
+			logrus.Infof("%.1f%% done", float64(lTW)/10_000_000)
+		}
+	}
 }
